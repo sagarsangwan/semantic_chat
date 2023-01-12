c09e963792d9880e5756a5554f876c506c9da3d6
sagar sangwan <iamsagarsangwan@gmail.com>
2023-01-12 18:45:21 +0530

    added urls and templates page

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import include
+from django.shortcuts import render
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'landing_page/login.html')
+
+
+@login_required
+def home(request):
+    return render(request, 'home/home.html')
