2665f3b3f8be991fd667f50f93721f21e2941436
sagar sangwan <iamsagarsangwan@gmail.com>
2023-01-15 16:39:53 +0530

    changes static file url

--- semantic_chat/settings/prod_settings.py
+++ semantic_chat/settings/prod_settings.py
@@ -6,10 +6,13 @@ load_dotenv('../.env')
 # STATIC_ROOT
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
