d0a7b053752a107a3d895ecf4cc0917ece72e92f
sagar sangwan <iamsagarsangwan@gmail.com>
2023-02-12 17:15:19 +0530

    ??????

--- semantic_chat/settings/local_settings.py
+++ semantic_chat/settings/local_settings.py
@@ -1,6 +1,6 @@
 from .base_settings import *
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
