7ded9829ad4d86572321a39bdbb7a4e89ae1861b
sagar sangwan <iamsagarsangwan@gmail.com>
2023-03-01 03:23:39 +0530

    user is now searchable

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
 # Create your models here.
+from django.urls import reverse
 
 
 class ChatRoomManager(models.Manager):
@@ -18,13 +19,23 @@ class ChatRoom(models.Model):
         User, on_delete=models.CASCADE, related_name='second_person', null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     objects = ChatRoomManager()
 
     class Meta:
         unique_together = ('first_person', 'second_person')
 
+    def get_absolute_url(self):
+        return reverse('chat:chatroom', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.first_person.username + ' - ' + self.second_person.username
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.first_person.username + '-' + \
+                self.second_person.username + '-' + str(self.pk)
+        super(ChatRoom, self).save(*args, **kwargs)
 
 
 class Message(models.Model):
@@ -32,6 +43,12 @@ class Message(models.Model):
     room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
     message = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.user.username + self.room.slug + str(self.pk)
+        super(Message, self).save(*args, **kwargs)
--- core/views.py
+++ core/views.py
@@ -3,13 +3,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import User, ChatRoom, Message, ChatRoomManager
-
+from django.db.models import Q
 # def login(request):
 #     return render(request, 'landing_page/login.html')
 
 
 @login_required
 def home(request):
+    search = request.GET.get('search')
+    if search:
+        all_users = User.objects.filter(
+            Q(username__icontains=search) | Q(email__icontains=search))
+        # exclude all superusers
+        all_users = all_users.exclude(is_superuser=True)
+        print(all_users)
+        context = {
+            'all_users': all_users
+        }
+        return render(request, 'pages/search_result.html', context)
     all_chat_rooms = ChatRoom.objects.by_user(user=request.user)
     print(all_chat_rooms)
     context = {
