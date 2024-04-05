3013291ec6e5e94b8260bbf797bdeb83c217f8a7
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-05 22:33:53 +0530

    changes in setting file

--- semantic_backend/settings/base_settings.py
+++ semantic_backend/settings/base_settings.py
@@ -116,7 +116,4 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
-
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- semantic_backend/settings/prod_settings.py
+++ semantic_backend/settings/prod_settings.py
@@ -1,3 +1,6 @@
+from semantic_backend.settings.base_settings import BASE_DIR
+import os
+
 DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "vercel.app"]
 CORS_ALLOWED_ORIGINS = [
@@ -5,3 +8,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 DATABASES = {}
+
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
