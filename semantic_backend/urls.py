5ddf5a075129aca41f829936c08881127f1370d1
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-05 22:35:49 +0530

    added path for static

--- semantic_backend/urls.py
+++ semantic_backend/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from semantic_backend import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,3 +10,6 @@ urlpatterns = [
         "auth/", include("drf_social_oauth2.urls", namespace="drf")
     ),  # add this line to include the auth urls
 ]
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
