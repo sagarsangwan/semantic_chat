c212c141d025f6ef4220958608455be000873acf
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-05 23:11:27 +0530

    added --no-input in buildsh_files

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,13 @@
+ echo "BUILD START"
+ # python3.9 -m pip install -r requirements.txt
+ python3.9 -m venv venv
+    source venv/bin/activate
+ pip install -r requirements.txt
+ # python3.9 manage.py collectstatic
+ # ls
+ echo "BUILD END"
+# source venv/bin/activate
 
-pip install -r requirements.txt
-echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-
-python3.9 manage.py migrate
+ # Collect static files
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
