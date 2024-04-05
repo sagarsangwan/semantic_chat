1c1e166ea961e0300a6fac9ac8c77f7985d82051
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-05 22:56:31 +0530

    update in  build file

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,8 @@
- echo "BUILD START"
- # python3.9 -m pip install -r requirements.txt
- python3.9 -m venv venv
-    source venv/bin/activate
- pip install -r requirements.txt
- # python3.9 manage.py collectstatic
- # ls
- echo "BUILD END"
-# source venv/bin/activate
 
- # Collect static files
+pip install -r requirements.txt
+echo "Making migrations..."
+python3.9 manage.py makemigrations --noinput
+
+python3.9 manage.py migrate
 echo "Collecting static files..."
-python manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --noinput --clear
