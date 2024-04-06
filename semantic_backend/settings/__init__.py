3073548397936bf169febc1e5df07670ef1521c0
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-06 17:27:19 +0530

    ??

--- semantic_backend/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base_settings import *
-import os
-
-if os.environ["mod"] == "production":
-    from .prod_settings import *
-
-else:
-    from .local_settings import *
--- semantic_backend/urls.py
+++ semantic_backend/urls.py
@@ -6,9 +6,9 @@ from semantic_backend import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "auth/", include("drf_social_oauth2.urls", namespace="drf")
-    ),  # add this line to include the auth urls
+    #     path(
+    #         "auth/", include("drf_social_oauth2.urls", namespace="drf")
+    #     ),  # add this line to include the auth urls
 ]
 # add at the last
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)/
