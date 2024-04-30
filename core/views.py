195bad4cb8623bb38da45fffdaa9fa84c5ff4778
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-30 23:22:29 +0530

    changes according vercel

--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
+# example/views.py
+from datetime import datetime
+
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+    now = datetime.now()
+    html = f"""
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
