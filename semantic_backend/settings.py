c8a1871a5c1fe9102ec388f191e741f0d1246eb6
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-30 12:14:27 +0530

    changes in setting according to vercel

--- semantic_backend/settings.py
+++ semantic_backend/settings.py
@@ -15,15 +15,15 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "sssssssssssssssssssssssssss"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
