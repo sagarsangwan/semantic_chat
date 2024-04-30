ff25758d68672da0b912f32ce0750324b8b402a2
sagar sangwan <iamsagarsangwan@gmail.com>
2024-04-30 11:41:35 +0530

    added new project

--- semantic_backend/urls.py
+++ semantic_backend/urls.py
@@ -1,15 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+"""
+URL configuration for semantic_backend project.
 
-from semantic_backend import settings
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-    #     path(
-    #         "auth/", include("drf_social_oauth2.urls", namespace="drf")
-    #     ),  # add this line to include the auth urls
+    path('admin/', admin.site.urls),
 ]
-# add at the last
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)/
